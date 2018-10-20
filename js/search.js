d4910f0c5da541c9aed7125b6a6ce5fa9c39e3ed
zhoupeijun <zhou1050253145@163.com>
2018-10-21 00:22:42 +0800

    完成搜索页，进入搜索展示页

---
+++ js/search.js
@@ -0,0 +1,23 @@
+$(function () {
+    $(".search_history dd").on("click",".close",function () {
+        console.log(this);
+
+    });
+
+    $(".search_history dt").on("click",".clearAll",function () {
+        console.log(this);
+    });
+    
+    $("#searchCommit").on("click",function () {
+        // console.log($(".searchBar .searchInput").val());
+        var key =$(".searchBar .searchInput").val().trim();
+        if (!key||key.length==0) {
+            mui.toast('请输入关键字',{ duration:1000, type:'div' });
+            return;
+        }
+        location.href="searchList.html?key="+key;
+    });
+
+
+});
+
